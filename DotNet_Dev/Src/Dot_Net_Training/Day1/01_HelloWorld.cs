d7fc8e55b30a9597336f68911ac31e9f3f84df1e
Yogesh Dhanapal <geoyogesh@gmail.com>
2014-02-01 00:17:15 +0530

    Added HelloWorld Project

---
+++ DotNet_Dev/Src/Dot_Net_Training/Day1/01_HelloWorld.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using ESRI.ArcGIS.ADF.BaseClasses;
+using ESRI.ArcGIS.ADF.CATIDs;
+using ESRI.ArcGIS.Framework;
+using ESRI.ArcGIS.ArcMapUI;
+using System.Windows.Forms;
+
+namespace Day1
+{
+    /// <summary>
+    /// Summary description for _01_HelloWorld.
+    /// </summary>
+    [Guid("51f8e437-bb22-42ed-8d1e-4d73ad8d930f")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ProgId("Day1._01_HelloWorld")]
+    public sealed class _01_HelloWorld : BaseCommand
+    {
+        #region COM Registration Function(s)
+        [ComRegisterFunction()]
+        [ComVisible(false)]
+        static void RegisterFunction(Type registerType)
+        {
+            // Required for ArcGIS Component Category Registrar support
+            ArcGISCategoryRegistration(registerType);
+
+            //
+            // TODO: Add any COM registration code here
+            //
+        }
+
+        [ComUnregisterFunction()]
+        [ComVisible(false)]
+        static void UnregisterFunction(Type registerType)
+        {
+            // Required for ArcGIS Component Category Registrar support
+            ArcGISCategoryUnregistration(registerType);
+
+            //
+            // TODO: Add any COM unregistration code here
+            //
+        }
+
+        #region ArcGIS Component Category Registrar generated code
+        /// <summary>
+        /// Required method for ArcGIS Component Category registration -
+        /// Do not modify the contents of this method with the code editor.
+        /// </summary>
+        private static void ArcGISCategoryRegistration(Type registerType)
+        {
+            string regKey = string.Format("HKEY_CLASSES_ROOT\\CLSID\\{{{0}}}", registerType.GUID);
+            MxCommands.Register(regKey);
+
+        }
+        /// <summary>
+        /// Required method for ArcGIS Component Category unregistration -
+        /// Do not modify the contents of this method with the code editor.
+        /// </summary>
+        private static void ArcGISCategoryUnregistration(Type registerType)
+        {
+            string regKey = string.Format("HKEY_CLASSES_ROOT\\CLSID\\{{{0}}}", registerType.GUID);
+            MxCommands.Unregister(regKey);
+
+        }
+
+        #endregion
+        #endregion
+
+        private IApplication m_application;
+        public _01_HelloWorld()
+        {
+            //
+            // TODO: Define values for the public properties
+            //
+            base.m_category = "Day1_Training"; //localizable text
+            base.m_caption = "HelloWorld";  //localizable text
+            base.m_message = "Displaying helloworld alert";  //localizable text 
+            base.m_toolTip = "Displaying Helloworld Message";  //localizable text 
+            base.m_name = "Day1_training_helloworld";   //unique id, non-localizable (e.g. "MyCategory_ArcMapCommand")
+
+            try
+            {
+                //
+                // TODO: change bitmap name if necessary
+                //
+                string bitmapResourceName = GetType().Name + ".bmp";
+                base.m_bitmap = new Bitmap(GetType(), bitmapResourceName);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.WriteLine(ex.Message, "Invalid Bitmap");
+            }
+        }
+
+        #region Overridden Class Methods
+
+        /// <summary>
+        /// Occurs when this command is created
+        /// </summary>
+        /// <param name="hook">Instance of the application</param>
+        public override void OnCreate(object hook)
+        {
+            if (hook == null)
+                return;
+
+            m_application = hook as IApplication;
+
+            //Disable if it is not ArcMap
+            if (hook is IMxApplication)
+                base.m_enabled = true;
+            else
+                base.m_enabled = false;
+
+            // TODO:  Add other initialization code
+        }
+
+        /// <summary>
+        /// Occurs when this command is clicked
+        /// </summary>
+        public override void OnClick()
+        {
+            MessageBox.Show("Helloworld");
+        }
+
+        #endregion
+    }
+}
---
+++ DotNet_Dev/Src/Dot_Net_Training/Day1/E02_RemoveAll_Layers.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using ESRI.ArcGIS.ADF.BaseClasses;
+using ESRI.ArcGIS.ADF.CATIDs;
+using ESRI.ArcGIS.Framework;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using System.Windows.Forms;
+
+namespace Day1
+{
+    /// <summary>
+    /// Summary description for E02_RemoveAll_Layers.
+    /// </summary>
+    [Guid("27c24747-c21e-4e7b-b5d3-c8606978b70e")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ProgId("Day1.E02_RemoveAll_Layers")]
+    public sealed class E02_RemoveAll_Layers : BaseCommand
+    {
+        #region COM Registration Function(s)
+        [ComRegisterFunction()]
+        [ComVisible(false)]
+        static void RegisterFunction(Type registerType)
+        {
+            // Required for ArcGIS Component Category Registrar support
+            ArcGISCategoryRegistration(registerType);
+
+            //
+            // TODO: Add any COM registration code here
+            //
+        }
+
+        [ComUnregisterFunction()]
+        [ComVisible(false)]
+        static void UnregisterFunction(Type registerType)
+        {
+            // Required for ArcGIS Component Category Registrar support
+            ArcGISCategoryUnregistration(registerType);
+
+            //
+            // TODO: Add any COM unregistration code here
+            //
+        }
+
+        #region ArcGIS Component Category Registrar generated code
+        /// <summary>
+        /// Required method for ArcGIS Component Category registration -
+        /// Do not modify the contents of this method with the code editor.
+        /// </summary>
+        private static void ArcGISCategoryRegistration(Type registerType)
+        {
+            string regKey = string.Format("HKEY_CLASSES_ROOT\\CLSID\\{{{0}}}", registerType.GUID);
+            MxCommands.Register(regKey);
+
+        }
+        /// <summary>
+        /// Required method for ArcGIS Component Category unregistration -
+        /// Do not modify the contents of this method with the code editor.
+        /// </summary>
+        private static void ArcGISCategoryUnregistration(Type registerType)
+        {
+            string regKey = string.Format("HKEY_CLASSES_ROOT\\CLSID\\{{{0}}}", registerType.GUID);
+            MxCommands.Unregister(regKey);
+
+        }
+
+        #endregion
+        #endregion
+
+        private IApplication m_application;
+        public E02_RemoveAll_Layers()
+        {
+            //
+            // TODO: Define values for the public properties
+            //
+            base.m_category = "Day1_Training"; //localizable text
+            base.m_caption = "Remove All Layers";  //localizable text
+            base.m_message = "Remove All Layers from Map Document";  //localizable text 
+            base.m_toolTip = "Remove All Layers from Map Document";  //localizable text 
+            base.m_name = "Day1_training_remove_all_layers";   //unique id, non-localizable (e.g. "MyCategory_ArcMapCommand")
+
+            try
+            {
+                //
+                // TODO: change bitmap name if necessary
+                //
+                string bitmapResourceName = GetType().Name + ".bmp";
+                base.m_bitmap = new Bitmap(GetType(), bitmapResourceName);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.WriteLine(ex.Message, "Invalid Bitmap");
+            }
+        }
+
+        #region Overridden Class Methods
+
+        /// <summary>
+        /// Occurs when this command is created
+        /// </summary>
+        /// <param name="hook">Instance of the application</param>
+        public override void OnCreate(object hook)
+        {
+            if (hook == null)
+                return;
+
+            m_application = hook as IApplication;
+
+            //Disable if it is not ArcMap
+            if (hook is IMxApplication)
+                base.m_enabled = true;
+            else
+                base.m_enabled = false;
+
+            // TODO:  Add other initialization code
+        }
+
+        /// <summary>
+        /// Occurs when this command is clicked
+        /// </summary>
+        public override void OnClick()
+        {
+            IMapDocument pMapDocument = (IMapDocument)m_application.Document;
+
+            DeleteAllDataFrames(pMapDocument);
+        }
+
+        private void DeleteAllDataFrames(IMapDocument pMapDocument)
+        {
+            for (int i = 0; i < pMapDocument.MapCount; i++)
+            {
+
+            }
+        }
+
+        #endregion
+    }
+}
